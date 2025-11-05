7140d13c2afd5279ebb88cf6d34e8caae553cc93
unknown <charan.maraboina@analogueitsolutionz.com>
2025-11-05 13:07:31 +0530

    implemented the schemas and made the chnages

--- models/organisationAdminModel.js
+++ models/organisationAdminModel.js
@@ -1,15 +1,17 @@
 const mongoose=require("mongoose");
 
 const organisationAdminSchema=new mongoose.Schema({
-        email: { type: String, required: false, unique: true },
+    email: { type: String, required: false, unique: true },
     password: { type: String, required: false, default: "" },
     otp: { type: Number, required: false },
     otpVerified: { type: Boolean, default: false, required: false },
     status: { type: String, default: "Active", required: false },
     name: { type: String, default: "Organisation Admin" },
-    roles: { type: mongoose.Schema.Types.ObjectId, ref: "roles" },
+    roles: [{ type: mongoose.Schema.Types.ObjectId, ref: "roles" }],
     
-})
+},
+  { timestamps: true }
+)
 
 const organisationadmin=mongoose.model("organisationadmin",organisationAdminSchema);
 
@@ -17,19 +19,21 @@ const organisationadmin=mongoose.model("organisationadmin",organisationAdminSche
 const rolesSchema=new mongoose.Schema({
         roleName: { type: String, required: false, unique: true },
     status: { type: String, default: "Active", required: false },
-        modules: { type: mongoose.Schema.Types.ObjectId, ref: "modules" },
+        modules: [{ type: mongoose.Schema.Types.ObjectId, ref: "modules" }],
     
-})
+},
+  { timestamps: true })
 
-const roles=mongoose.model("organisationadmin",rolesSchema);
+const roles=mongoose.model("roles",rolesSchema);
 
 
 const moduleSchema=new mongoose.Schema({
     moduleName: { type: String, required: false, unique: true },
     status: { type: String, default: "Active", required: false },
-        priveleages: { type: mongoose.Schema.Types.ObjectId, ref: "priveleages" },
+        priveleages: [{ type: mongoose.Schema.Types.ObjectId, ref: "priveleages" }],
     
-})
+},
+  { timestamps: true })
 
 const modules=new mongoose.model("modules",moduleSchema);
 
@@ -37,10 +41,11 @@ const modules=new mongoose.model("modules",moduleSchema);
 const privelagesSchema=new mongoose.Schema({
     priveleageName: { type: String, required: false, unique: true },
     status: { type: String, default: "Active", required: false },
-         slug: { type: mongoose.Schema.Types.ObjectId, ref: "slug" },
+         slug: [{ type: mongoose.Schema.Types.ObjectId, ref: "slug" }],
 
     
-})
+},
+  { timestamps: true })
 
 const priveleages=new mongoose.model("priveleages",privelagesSchema);
 
@@ -50,13 +55,14 @@ const priveleages=new mongoose.model("priveleages",privelagesSchema);
 const slugSchema=new mongoose.Schema({
     name: { type: String, required: false, unique: true },
     slugName: { type: String, required: false, unique: true },
-    status: { type: String, default: "Active", required: false },
+    status: [{ type: String, default: "Active", required: false }],
     
-})
+},
+  { timestamps: true })
 
 const slug=new mongoose.model("slug",slugSchema);
 
 
 
-module.exports={organisationAdminScehma,roles};
+module.exports={organisationadmin,roles,modules,priveleages,slug};
 
